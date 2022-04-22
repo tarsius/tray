0ff79f738fbff38520d329aa93d59678cfc0e55f
Jonas Bernoulli <jonas@bernoul.li>
2022-04-22 18:28:10 +0200

    Refresh library header

--- tray.el
+++ tray.el
@@ -1,27 +1,27 @@
-;;; tray.el --- Various transient menus            -*- lexical-binding: t -*-
+;;; tray.el --- Various transient menus  -*- lexical-binding:t -*-
 
-;; Copyright (C) 2021-2022  Jonas Bernoulli
+;; Copyright (C) 2021-2022 Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://git.sr.ht/~tarsius/tray
-;; Keywords: compile, convenience, lisp
+;; Keywords: convenience
 
 ;; Package-Requires: ((emacs "27.1") (compat "28.1.1.0") (transient "0.3.0"))
 
-;; This file is not part of GNU Emacs.
+;; SPDX-License-Identifier: GPL-3.0-or-later
 
-;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
-
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
+;;
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
-;; For a full copy of the GNU General Public License
-;; see <http://www.gnu.org/licenses/>.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
