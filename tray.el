15c08ec2b57ce0535f54242088d6051d2cf9a049
Jonas Bernoulli <jonas@bernoul.li>
2024-01-01 23:26:14 +0100

    Bump copyright years

--- tray.el
+++ tray.el
@@ -1,6 +1,6 @@
 ;;; tray.el --- Various transient menus  -*- lexical-binding:t -*-
 
-;; Copyright (C) 2021-2023 Jonas Bernoulli
+;; Copyright (C) 2021-2024 Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://git.sr.ht/~tarsius/tray
