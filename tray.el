1ce8c7c74dec199e1045fec3c79cb9ff433b1735
Jonas Bernoulli <jonas@bernoul.li>
2024-04-15 17:49:12 +0200

    Update contact information

--- tray.el
+++ tray.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2021-2024 Jonas Bernoulli
 
-;; Author: Jonas Bernoulli <jonas@bernoul.li>
+;; Author: Jonas Bernoulli <emacs.tray@jonas.bernoulli.dev>
 ;; Homepage: https://git.sr.ht/~tarsius/tray
 ;; Keywords: convenience
 
