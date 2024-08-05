988afc5967237ed9acfd6d9a53c631e476fccc62
Jonas Bernoulli <jonas@bernoul.li>
2024-08-05 16:38:51 +0200

    Release version 0.1.4

--- tray.el
+++ tray.el
@@ -6,6 +6,7 @@
 ;; Homepage: https://git.sr.ht/~tarsius/tray
 ;; Keywords: convenience
 
+;; Package-Version: 0.1.4
 ;; Package-Requires: ((emacs "27.1") (compat "30.0.0.0") (transient "0.7.0"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
